9d624e2b69197372c69b4609694b73ffceb9cd2f
GeoArchonMorax <korisawarezero@gmail.com>
2024-10-23 22:01:44 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,29 @@
+
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
   name: 'help',
   description: 'Show available commands',
-  author: 'System',
+  author: 'Aljur Pogoy',
   execute(senderId, args, pageAccessToken, sendMessage) {
     const commandsDir = path.join(__dirname, '../commands');
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
     const commands = commandFiles.map(file => {
       const command = require(path.join(commandsDir, file));
-      return `⚔️ ${command.name}\n  - ${command.description}\n  - Credits: ${command.author}`;
+      return `➯《 ${command.name} 》\n  ➯ ${command.description}\n  ➯ Credits: 𝙼𝚊𝚛𝚓𝚑𝚞𝚗 𝙱𝚊𝚢𝚕𝚘𝚗`;
     });
 
     const totalCommands = commandFiles.length;
-    const helpMessage = `Here are the available commands: \nTotal commands: ${totalCommands} \n\n${commands.join('\n\n')}`;
-    
+    const pageSize = 5;
+    const page = parseInt(args[0]) || 1;
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+    const paginatedCommands = commands.slice(start, end);
+
+    const helpMessage = `Kazuto Kirigaya Commands\n━━━━━━━━━━━━━━━━━\n${paginatedCommands.join('\n\n')}\n\nPage ${page} of ${Math.ceil(totalCommands / pageSize)}\n━━━━━━━━━━━━━━━━━\n𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 : 𝙼𝙰𝚁𝙹𝙷𝚄𝙽 𝙱𝙰𝚈𝙻𝙾𝙽`;
+
     sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
 };
